da8b526b87d21cb040032c1efa54c67c008d9529
mrodriguezto <mirdor.dev@gmail.com>
2022-11-01 16:47:35 -0500

    fixes: commentary model fields

--- app/models/commentary.py
+++ app/models/commentary.py
@@ -1,31 +1,23 @@
 from datetime import datetime
-from mongoengine import Document, StringField, DateField, EmbeddedDocumentListField, ListField, EmbeddedDocumentField
+from bson.objectid import ObjectId
 
-from .commentary import Commentary
+from mongoengine import EmbeddedDocument, StringField, DateField, ObjectIdField
 
 
-class Publication(Document):
+class Commentary(EmbeddedDocument):
     '''
-    Class to define Publication Model
+    Class to define Commentary Embedded Model
     '''
+    _id = ObjectIdField(required=True, default=ObjectId)
     author_id = StringField(required=True)
-    title = StringField(required=True, max_length=100)
-    content = StringField(required=True, max_length=256)
-    img_url = StringField(required=False, default="")
-    img_public_id = StringField(required=False, default="")
-    hashtags = ListField(StringField())
-    commentaries = EmbeddedDocumentListField(Commentary)
+    commentary = StringField(required=True, max_length=100)
     created_at = DateField(default=datetime.utcnow)
 
-    def to_json(self):
-        publication_dict = {
-            "publication_id": str(self.pk),
-            "title": self.title,
-            "content": self.content,
+    def to_dict(self):
+        commentary_dict = {
+            "commentary_id": str(self._id),
             "author_id": self.author_id,
-            "img_url": self.img_url,
-            "created_at": self.created_at,
-            "hashtags": self.hashtags,
-            "commentaries": list(map(lambda commentary: commentary.to_dict(), self.commentaries))
+            "commentary": self.commentary,
+            "created_at": self.created_at
         }
-        return publication_dict
+        return commentary_dict
