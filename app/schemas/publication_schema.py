b82e496aa01042842c80bc5632eff746057b341e
Leonardo Torres <dleonardotb2001@gmail.com>
2022-12-27 17:43:12 -0500

    added validation pydantic

--- app/schemas/publication_schema.py
+++ app/schemas/publication_schema.py
@@ -1,9 +1,9 @@
 '''Publication Schemas'''
 from datetime import date
+from bson import ObjectId
 from typing import List, Optional
 
-from pydantic import BaseModel
-
+from pydantic import BaseModel, validator
 
 class Commentary(BaseModel):
 
@@ -13,6 +13,17 @@ class Commentary(BaseModel):
     author_id: str
     commentary: str
     created_at: date
+
+    @validator('commentary')
+    def commentary_must_not_be_null(cls, v):
+        if v is None:
+            raise ValueError("Commentary must not be Null")
+        return v.title()
+
+    @validator("author_id")
+    def author_id_must_be_object_id(cls, v):
+        assert ObjectId.is_valid(v)
+        return v
 
 
 class Publication(BaseModel):
@@ -27,3 +38,20 @@ class Publication(BaseModel):
     hashtags: List[str]
     commentaries: Optional[List[Commentary]] = None
     created_at: date
+
+    @validator('title')
+    def title_must_not_be_null(cls, v):
+        if v is None:
+            raise ValueError("Title must not be Null")
+        return v.title()
+
+    @validator('content')
+    def content_must_not_be_null(cls, v):
+        if v is None:
+            raise ValueError("Content must not be Null")
+        return v.title()
+
+    @validator("author_id")
+    def author_id_must_be_object_id(cls, v):
+        assert ObjectId.is_valid(v)
+        return v
